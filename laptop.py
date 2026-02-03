fadd35454be5d21e1d73e0d3a0a9c563b2e98dd2
dkkavana-spec <dkkavana9@gmail.com>
2026-02-03 13:01:28 +0530

    fghrbg

---
+++ laptop.py
@@ -0,0 +1,7 @@
+item_name = "laptop"
+quantity = 2
+price = 499.99
+in_stock = True
+print(f"item: {item_name}, qty: {quantity}, price: {price}, available: {in_stock}")
+total_cost = quantity * price
+print(f"total cost: {total_cost}")
